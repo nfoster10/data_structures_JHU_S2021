942b16b1d1c1c5a4eb9e8ba285b979d43b973c49
John Doe <jdoe@purpleLaptop.local>
2021-03-01 18:46:27 -0500

    lab1 got closer to conversion thanks to a friend:)

--- src_java/lab1/PrefixToPostfixConverter.java
+++ src_java/lab1/PrefixToPostfixConverter.java
@@ -1,7 +1,7 @@
 public class PrefixToPostfixConverter
 {
-	private static final char [] operators 
-		= {'-', '+', '/', '*', '$'}; 
+	private static final String [] operators 
+		= {"-", "+", "/", "*", "$"}; 
 	/*
 	input
 	precondition
@@ -12,46 +12,32 @@ public class PrefixToPostfixConverter
 	public boolean prefixToPostfix(String inputEquation, StringBuilder outputEquation)
 	{
 		boolean success = true;
-		Stack alpha = new Stack(inputEquation.length());
-		char inputChar;
-		Character pop1;
-		Character pop2;
+		Stack symbolStack = new Stack(inputEquation.length());
+		Stack operatorStack = new Stack(inputEquation.length()/2 + 1);
+		//char inputChar;
+		String inputChar;
+		//Character pop1;
+		String pop1;
+		//Character pop2;
+		String pop2;
 
 		for (int i = inputEquation.length() - 1; i >= 0; i--)
 		{
-			inputChar = inputEquation.charAt(i);
-			if (isOperator(inputChar))
-			{
-				if (alpha.isEmpty())
-				{	
-					success = false;
-					break;
-				}
-				else
-				{	
-					//System.out.println("AAA");
-					pop1 = (Character) alpha.pop();
-				}
+			inputChar = Character.toString(inputEquation.charAt(i));
 
-				if (alpha.isEmpty())
-				{
-					outputEquation.insert(0,pop1.charValue());
-					outputEquation.append(String.format("%c",inputChar));
-					//break;
-				}
-				else
-				{
-					pop2 = (Character) alpha.pop();
-					//System.out.println(pop1);
-					outputEquation.append(String.format("%c%c%c",pop2.charValue(),pop1.charValue(),inputChar));
-				}
+			if(isOperator(inputChar))
+			{
+				pop1 = (String) symbolStack.pop();
+				pop2 = (String) symbolStack.pop();
+
+				symbolStack.push(String.format("%s%s%s",pop1, pop2, inputChar));	
 			}
 			else
 			{
-				//System.out.println("BBB");
-				alpha.push(inputChar);
+				symbolStack.push(inputChar);
 			}
-		}	
+		}
+		outputEquation.append(symbolStack.pop());	
 
 		return success;
 	}
@@ -64,13 +50,109 @@ public class PrefixToPostfixConverter
 	postcondition
 	output
 	*/
-	private static boolean isOperator(char inputChar)
+	private static boolean isOperator(String inputChar)
 	{
 		boolean inputIsOperator = false;
-		for (char operator : operators)
-			if (inputChar == operator)
+		for (String operator : operators)
+			if (inputChar.equals(operator))
 				inputIsOperator = true;
 
 		return inputIsOperator;
 	}
 }
+
+
+			/*
+			if (isOperator(inputChar))
+			{
+				if(outputEquation.length() == 0)
+				{
+					pop1 = (Character) symbolStack.pop();
+					pop2 = (Character) symbolstack.pop();
+
+					outputEquation.append(String.format("%c%c%c",pop1.charValue(), pop2.charValue(), inputChar));
+				}
+				else if (outputEquation.charAt(inputEquation.length() - i))
+				{//TODO: operator from input is higher order than last out
+					outputEquation.insert(0, pop1.charValue());
+					outputEquation.append(inputChar);
+				}
+				else
+				{//TODO: operator form input is lower order
+					operatorStack.push(inputChar);
+				}
+			}
+			else
+			{
+				//TODO: add check for valid letter
+				symbolStack.push(inputChar);
+			}*/
+			/*if (isOperator(inputChar))
+			{
+				if(symbolStack.isEmpty())
+				{
+					//operator reached but no operands
+					success = false;
+					break;
+				}
+				else
+				{
+					pop1 = (Character) symbolStack.pop();
+					
+					if(outputEquation.length() == 0)
+					{
+						if(symbolStack.isEmpty())
+						{
+							//operator reached and no operand on stack or outuput
+							success = false;
+							break;
+						}
+						else
+						{
+							//first operator so must pop a second time
+							pop2 = (Character) symbolStack.pop();
+							outputEquation.append(String.format("%c%c%c",pop1.charValue(), pop2.charValue(), inputChar));
+						}
+					}
+					else //use output as operand
+					{
+						outputEquation.insert(0, pop1.charValue());
+						outputEquation.append(inputChar);
+					}
+					
+				}
+			}
+			else
+				symbolStack.push(inputChar);*/
+
+			/*if (isOperator(inputChar))
+			{
+				if (symbolStack.isEmpty())
+				{	
+					success = false;
+					break;
+				}
+				else
+				{	
+					//System.out.println("AAA");
+					pop1 = (Character) symbolStack.pop();
+				}
+
+				if (symbolStack.isEmpty())
+				{
+					outputEquation.insert(0,pop1.charValue());
+					outputEquation.append(String.format("%c",inputChar));
+					//break;
+				}
+				else
+				{
+					pop2 = (Character) symbolStack.pop();
+					//System.out.println(pop1);
+					outputEquation.append(String.format("%c%c%c",pop2.charValue(),pop1.charValue(),inputChar));
+				}
+			}
+			else
+			{
+				//System.out.println("BBB");
+				symbolStack.push(inputChar);
+			}*/
